e0fd6482d4041d816430ec2d714b69c9ba7ff042
Alan Repositorio <alanguevara.co@gmail.com>
2023-08-16 14:21:37 -0300

    2do commit

--- db.js
+++ db.js
@@ -1,11 +1,27 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Ms_MariaDB_Des',
-  database: 'productos',
-  port:3399
+const connection = mysql.createPool({
+  host: 'mysql-catok2.alwaysdata.net',
+  user: 'catok2_k',
+  password: 'astrologo2',
+  database: 'catok2_productos_items',
+  port:3306,
+  waitForConnections: true,
+  connectionLimit : 10,
+  queueLimit:0
+})
+
+connection.getConnection((error,connection) =>{
+
+  if(error){
+    console.error("Hubo un error de conexión", error);
+  }else{
+    console.log("Conexión a la base de datos fue exitosa.");
+    connection.release();
+  }
+
 });
 
-module.exports = connection;
+module.exports = {
+  db: connection.promise()
+  };
